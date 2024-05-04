843013ac78cc9cefdb429bdf234895e0ca53ce1a
dzarkV <dfvargasp@unal.edu.co>
2024-05-04 11:22:25 -0500

    allow front url in cors & added it env var

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.v1.post_suggestions import router as v1_router
+from dotenv import load_dotenv
+
+
+load_dotenv()
+FRONT_END_URL = os.getenv("FRONT_END_URL")
 
 app = FastAPI()
-
-
 app.include_router(v1_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONT_END_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/healthcheck")
