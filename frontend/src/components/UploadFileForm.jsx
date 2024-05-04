ce6dea2beca3c1c62fad6bd6c972d93b75edb61b
JVPH <gyrospizzeria@protonmail.com>
2024-05-03 21:02:36 -0500

    Implement notification component

--- frontend/src/components/UploadFileForm.jsx
+++ frontend/src/components/UploadFileForm.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 function UploadFileForm({
   handleFileUpload,
-  expectedFiles = ["clinical_history.pdf", "brain_mri.jpg"],
+  expectedFiles = ["clinical_history.pdf", "brain_mri.jpeg"],
+  notify,
 }) {
   const submitHandler = async (e) => {
     e.preventDefault();
     const files = e.target.elements.file.files;
 
     if (files.length === 0) {
-      throw new Error("No files selected");
+      notify("No files selected", "error");
+      return;
     }
 
     if (files.length !== expectedFiles.length) {
-      throw new Error(`Please upload exactly ${expectedFiles.length} files`);
+      console.log("hi");
+      notify(`Please upload exactly ${expectedFiles.length} files`, "error");
+      return;
     }
 
     const filenames = Array.from(files).map((file) => file.name);
@@ -21,16 +25,19 @@ function UploadFileForm({
     if (
       !expectedFiles.every((expectedFile) => filenames.includes(expectedFile))
     ) {
-      throw new Error("Wrong filenames");
+      notify("Wrong filenames", "error");
+      return;
     }
 
     const formData = new FormData();
 
     for (let i = 0; i < expectedFiles.length; i++) {
+      //To do, change key names
       formData.append("files", files[i]);
     }
 
     handleFileUpload(formData);
+    notify("Files successfully sent. Processing...");
   };
 
   return (
