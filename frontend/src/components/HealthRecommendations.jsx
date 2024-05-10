f7d9e5da1bc498ca998f9bcbafebb2bad26ec7c0
JVPH <gyrospizzeria@protonmail.com>
2024-05-10 14:11:16 -0500

    Use downloadPDF function

--- frontend/src/components/HealthRecommendations.jsx
+++ frontend/src/components/HealthRecommendations.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Markdown from "react-markdown";
+import { downloadPDF } from "../utils/downloadPDF";
 
 function HealthRecommendations({ data, isLoading }) {
+  const markdownRef = React.useRef();
 
   if (isLoading) {
     return (
@@ -12,9 +14,21 @@ function HealthRecommendations({ data, isLoading }) {
   if (!data) return null;
 
   return (
-    <div className="max-w-xl pl-16">
-      <p className="font-bold text-xl">{data.classification}</p>
-      <Markdown className="prose my-10">{data.analysis_results}</Markdown>
+    <div className="flex flex-col">
+      <button
+        onClick={() => downloadPDF(markdownRef.current)}
+        className="btn btn-secondary w-1/3 self-center m-4"
+      >
+        Descargar PDF
+      </button>
+      <div className="" ref={markdownRef}>
+        <p className="font-semibold text-xl max-w-xl m-4 italic">
+          {data.classification}
+        </p>
+        <Markdown className="prose my-10 m-auto">
+          {data.analysis_results}
+        </Markdown>
+      </div>
     </div>
   );
 }
