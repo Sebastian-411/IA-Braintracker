7c44164af36945352f2d87c5614aec42ff655bd8
JVPH <83098530+JVPH@users.noreply.github.com>
2024-05-10 21:10:52 -0500

    Develop (#54)
    * DOC: how to run in Readme
    
    * Fix styling issues
    
    * remove mock data
    
    * change title
    
    * Implement download results as pdf
    
    * Fix problem with fonts
    
    * Implement better solution
    
    ---------
    
    Co-authored-by: dzarkV <dfvargasp@unal.edu.co>
    Co-authored-by: Diego Vargas <91356068+dzarkV@users.noreply.github.com>

--- frontend/src/utils/downloadToPDF.jsx
+++ frontend/src/utils/downloadToPDF.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { htmlToText } from "html-to-text";
 import MarkdownIt from "markdown-it";
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
 
 export const downloadPDF = (markdownString) => {
-  pdfMake.vfs = pdfFonts.pdfMake.vfs;
+  pdfMake.fonts = {
+    Roboto: {
+      normal:
+        "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+      bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+      italics:
+        "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+      bolditalics:
+        "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+    },
+  };
+
   const md = new MarkdownIt();
   const htmlString = md.render(markdownString);
   const textString = htmlToText(htmlString, {
